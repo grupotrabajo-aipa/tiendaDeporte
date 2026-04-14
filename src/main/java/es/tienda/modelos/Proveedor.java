4339a89e7b405b934d63bd91881c92799a42a697
Idaira <isuaand2601@g.educaand.es>
2026-04-14 12:56:23 +0200

    He añadido la clase Proveedor y TipoProveedor, he refactorizado y hecho el javadoc.

---
+++ src/main/java/es/tienda/modelos/Proveedor.java
@@ -0,0 +1,48 @@
+package es.tienda.modelos;
+
+/**
+ * @author IdairaSuanez
+ */
+public class Proveedor extends Persona {
+
+    public static final String PROVEEDOR_DADO_DE_ALTA = "=== Proveedor dado de alta ===";
+    private TipoProveedor tipoProveedor;
+
+    /**
+     *
+     * @param nombre
+     * @param apellidos
+     * @param dni
+     * @param tipoProveedor
+     */
+    public Proveedor(String nombre, String apellidos, String dni, TipoProveedor tipoProveedor) {
+        super(nombre, apellidos, dni);
+        this.tipoProveedor = tipoProveedor;
+    }
+    /**
+     *
+     * @param nombre
+     * @param apellidos
+     * @param dni
+     * @param tipoProveedor
+     * @return
+     */
+    public static Proveedor altaProveedor(String nombre, String apellidos, String dni, TipoProveedor tipoProveedor) {
+        System.out.println(PROVEEDOR_DADO_DE_ALTA);
+        return new Proveedor(nombre, apellidos, dni, tipoProveedor);
+    }
+
+    @Override
+    public void mostrarDatos() {
+        System.out.printf("Nombre: %s%nApellidos: %s%nDNI: %s%nTipo de proveedor: %s%n",
+                getNombre(), getApellidos(), getDni(), tipoProveedor);
+    }
+
+    public TipoProveedor getTipoProveedor() {
+        return tipoProveedor;
+    }
+
+    public void setTipoProveedor(TipoProveedor tipoProveedor) {
+        this.tipoProveedor = tipoProveedor;
+    }
+}
